70df067051f50ff3890cae3a82031105e5d269d4
Megan Banaski <mbana641@uwsp.edu>
2019-04-04 08:49:23 -0500

    completed mods

--- completed.php
+++ completed.php
@@ -1,24 +1,18 @@
 <?php
-
 require_once("./assets/Template.php");
 require_once("./assets/DB.class.php");
-
 $page = new Template("CMNT Survey");
 $page->addHeadElement('<link rel="stylesheet" href="./assets/styles/normalize.css">');
 $page->addHeadElement('<link rel="stylesheet" type="text/css" href="./assets/styles/styles.css">');
 $page->addHeadElement('<link href="https://fonts.googleapis.com/css?family=Krub|PT+Sans|Ubuntu" rel="stylesheet">');
 $page->finalizeTopSection();
 $page->finalizeBottomSection();
-
 $db = new DB();
-
 if (!$db->getConnStatus()) {
   print "An error has occurred with connection\n";
   exit;
 }
-
 print $page->getTopSection();
-
 $inputs = array('major','grade','pizza');
 $error = false;
 foreach($inputs as $field) {
@@ -33,10 +27,12 @@ if($error) {
 	print "<p>All form fields required. Please try again.</p>";
 } else {
 	$major = filter_var($_POST["major"], FILTER_SANITIZE_STRING);
+	$major = $db->dbEsc($major);
 	$grade = filter_var($_POST["grade"], FILTER_SANITIZE_STRING);
+	$grade = $db->dbEsc($grade);
 	$pizza = filter_var($_POST["pizza"], FILTER_SANITIZE_STRING);
+	$pizza = $db->dbEsc($pizza);
 }
-
 if(isset($_SERVER['REMOTE_ADDR'])) {
 	$userip = filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP);
 }
@@ -44,9 +40,7 @@ if(isset($_SERVER['REMOTE_ADDR'])) {
 $sql = "INSERT INTO survey (submittime, major, expectedgrade, favetopping, userip)
 	VALUES (now(),'$major','$grade','$pizza','$userip')";
 	
-
 $db->dbCall($sql);
-
 print '
 <div class="paneContainer">
 <div class="pane">
@@ -60,6 +54,5 @@ print '
     </div>
 </div>
 </div>';
-
 print $page->getBottomSection();
 ?>
