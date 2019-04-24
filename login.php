b71180294d5b399d38c81938e2af3f33ddfea4d4
njb <njblondhe@gmail.com>
2019-04-23 19:38:18 -0500

    separate backend/frontend auth

--- login.php
+++ login.php
@@ -1,16 +1,6 @@
 <?php
-
-require_once("assets/DB.class.php");
 require_once("assets/Template.php");
-
-$db = new DB();
-
 session_start();
-
-if (!$db->getConnStatus()) {
-  print "An error has occurred with connection\n";
-  exit;
-}
 
 $page = new Template("Login");
 $page->addHeadElement('<link rel="stylesheet" href="./assets/styles/normalize.css">');
@@ -23,24 +13,43 @@ print $page->getTopSection();
 
 
 if(isset($_POST["username"]) && isset($_POST["password"])){
-    $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
-    $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-    $username = $db->dbEsc($username);
-    
-    $query = "SELECT *
-                    FROM user, role, user2role
-                    WHERE user.username='$username' and user.id=user2role.id and user2role.roleid=role.id";
-    $userResults = $db->dbCall($query);
-    if (sizeof($userResults) > 0) {
 
-        $validPass = password_verify($password, $userResults[0]['userpass']); 
-        
-        if ($validPass) {
-            $_SESSION['user'] = $userResults[0]['realname'];
-            $_SESSION['role'] = $userResults[0]['rolename'];
+    $dataJson = json_encode($_POST);
+    $postString = "data=" . urlencode($dataJson);
+    $contentLength = strlen($postString);
+
+    $header = array(
+    'Content-Type: application/x-www-form-urlencoded',
+    'Content-Length: ' . $contentLength
+    );
+
+    $url = "cnmtsrv2.uwsp.edu/~nblon447/sprint3/backend/post-authentication.php";
+    $ch = curl_init();
+
+    curl_setopt($ch,
+        CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch,
+        CURLOPT_POSTFIELDS, $postString);
+    curl_setopt($ch,
+        CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $returnData = curl_exec($ch);
+    $response = json_decode($returnData, true);
+    
+    if (!empty($response)) {        
+        if ($response['status'] == 'success') {
+            $_SESSION['user'] = $response['user'];
+            $_SESSIONS['roles'] = $response['roles'];
+        } else {
+            $userMessage = $response['message'];
         }
+    } else {
+        $userMessage = 'An error has occured. Please try again.';
     }
-        
+
+    curl_close($ch);
 }
 print '<div class="content">
 <header id="header">
@@ -63,7 +72,7 @@ print '<div class="content">
             {
                 echo '<li><a class="link navLink" href="./login.php"><div class="btn btn__text">Login</div></a></li>';
             }
-            if (isset($_SESSION['role']) && ($_SESSION['role'] == 'admin')) {
+            if (isset($_SESSION['roles']) && (in_array('admin', $_SESSION['roles']))) {
                 print '<li><a class="link navLink" href="./SurveyData.php"><div class="btn btn__text">DATA</div></a></li>';
             }
     print  '<li><a class="link navLink" href="./privacy.php"><div class="btn btn__text">PRIVACY</div></a></li>
@@ -89,11 +98,8 @@ if (isset($_SESSION['user'])) {
         </form>
     </div>';
 } else {
-
-        if(isset($username)) {
-            print '<span id="nullInputError" style="display: block;">Incorrect username or password. Please try again</span>';
-        } else {
-            print '<span id="nullInputError">Please enter your username and password.</span>';
+        if(isset($userMessage)) {
+            print '<span id="nullInputError" style="display: block;">' . $userMessage . '</span>';
         }
         print '<form name="loginForm"  onsubmit="return validateLogin()" action="./login.php" method="post"  class="question login_element">
             Username: <input type="text" name="username" id="username" autofocus >
